832cb052e1891a95dc874d0e4f72159fe7283cd6
Dmitry Ponyatov <dponyatov@gmail.com>
2016-03-13 13:54:30 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cpp.cpp
+++ cpp.cpp
@@ -1,7 +1,56 @@
 #include "hpp.hpp"
 #define YYERR "\n\n"<<yylineno<<":"<<msg<<"["<<yytext<<"]\n\n"
 void yyerror(string msg) { cout<<YYERR; cerr<<YYERR; exit(-1); }
-int main() { return yyparse(); }
+int main() { env_init(); return yyparse(); }
 
-string Sym::dump() { return "<"+val+">"; }
+Sym::Sym(string T, string V) { tag=T; val=V; }
+Sym::Sym(string V):Sym("",V) {}
+void Sym::push(Sym*o) { nest.push_back(o); }
 
+string Sym::dump(int depth) { string S = "\n" + pad(depth) + tagval();
+	for (auto it=nest.begin(),e=nest.end();it!=e;it++)
+		S += (*it)->dump(depth+1);
+	return S; }
+string Sym::pad(int n) { string S; for (int i=0;i<n;i++) S+='\t'; return S; }
+string Sym::tagval() { return "<"+tag+":"+val+">"; }
+string Sym::tagstr() { return "<"+tag+":'"+val+"'>"; }
+
+Sym* glob = new Sym("env","global");
+
+Sym* Sym::lookup(string V) {
+	auto E = par.find(V);
+	if (E!=par.end()) return E->second;
+	else return NULL;
+}
+
+Sym* Sym::eval(Sym*env) {
+	Sym* E = env->lookup(val); if (E) return E;
+	for (auto it=nest.begin(),e=nest.end();it!=e;it++)
+		(*it) = (*it)->eval(env);
+	return this; }
+
+Sym* Sym::str() { return new Str(val); }
+Sym* Sym::at(Sym*o) { push(o); return this; }
+
+Str::Str(string V):Sym("str",V) {}
+string Str::tagval() { return tagstr(); }
+
+List::List():Sym("[","]") {}
+
+Op::Op(string V):Sym("op",V) {}
+Sym* Op::eval(Sym*env) {
+	Sym::eval(env);
+	if (val=="@") return nest[0]->at(nest[1]);
+	return this;
+}
+
+Fn::Fn(string V, FN F):Sym("fn",V) { fn=F; }
+Sym* Fn::at(Sym*o) { return fn(o); }
+
+File::File(string V):Sym("file",V) {}
+Sym* File::file(Sym*o) { return new File(o->str()->val); }
+string File::tagval() { return tagstr(); }
+
+void env_init() {
+	glob->par["file"] = new Fn("file",File::file);
+}
--- hpp.hpp
+++ hpp.hpp
@@ -3,13 +3,34 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <vector>
+#include <map>
 using namespace std;
 
 struct Sym {
-	string val;
-	Sym(string);
-	string dump();
+	string tag,val;
+	Sym(string,string); Sym(string);
+	virtual string tagval(); string tagstr(); 
+	string dump(int depth=0); string pad(int);
+	vector<Sym*> nest; void push(Sym*o);
+	map<string,Sym*> par; Sym* lookup(string);
+	virtual Sym* eval(Sym*env);
+	virtual Sym* str();
+	virtual Sym* at(Sym*);
 };
+extern Sym* glob;
+extern void env_init();
+
+struct Str: Sym { Str(string); string tagval(); };
+
+struct List: Sym { List(); };
+
+struct Op: Sym { Op(string); Sym*eval(Sym*); };
+
+typedef Sym*(*FN)(Sym*);
+struct Fn: Sym { Fn(string,FN); FN fn; Sym*at(Sym*); };
+
+struct File: Sym { File(string); static Sym* file(Sym*); string tagval(); };
 
 extern int yylex();
 extern int yylineno;
